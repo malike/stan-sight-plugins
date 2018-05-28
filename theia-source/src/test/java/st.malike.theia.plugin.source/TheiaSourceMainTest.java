50b0114a8c72d5fe538c02cc63fe5bfb74475826
malike <st.malike@gmail.com>
2018-05-28 23:51:12 +0000

    theia video source and video stream sink

---
+++ theia-source/src/test/java/st.malike.theia.plugin.source/TheiaSourceMainTest.java
@@ -0,0 +1,20 @@
+package st.malike.theia.plugin.source;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * @autor malike_st
+ */
+public class TheiaSourceMainTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+}
